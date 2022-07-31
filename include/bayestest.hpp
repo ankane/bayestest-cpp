4edf646147cfa433d979861a21f401e577cbc7fc
Andrew Kane <andrew@ankane.org>
2022-07-31 16:34:18 -0700

    Use std:: prefix for exp and log

--- include/bayestest.hpp
+++ include/bayestest.hpp
@@ -21,7 +21,7 @@ double prob_b_beats_a(int alpha_a, int beta_a, int alpha_b, int beta_b) {
   double beta_ba = beta_b + beta_a;
 
   for (auto i = 0; i < alpha_b; i++) {
-    total += exp(logbeta(alpha_a + i, beta_ba) - log(beta_b + i) - logbeta(1 + i, beta_b) - logbeta_aa_ba);
+    total += std::exp(logbeta(alpha_a + i, beta_ba) - std::log(beta_b + i) - logbeta(1 + i, beta_b) - logbeta_aa_ba);
   }
 
   return total;
@@ -36,7 +36,7 @@ double prob_c_beats_ab(int alpha_a, int beta_a, int alpha_b, int beta_b, int alp
   log_bb_j_logbeta_j_bb.reserve(alpha_b);
 
   for (auto j = 0; j < alpha_b; j++) {
-    log_bb_j_logbeta_j_bb.push_back(log(beta_b + j) + logbeta(1 + j, beta_b));
+    log_bb_j_logbeta_j_bb.push_back(std::log(beta_b + j) + logbeta(1 + j, beta_b));
   }
 
   double abc = beta_a + beta_b + beta_c;
@@ -48,10 +48,10 @@ double prob_c_beats_ab(int alpha_a, int beta_a, int alpha_b, int beta_b, int alp
   }
 
   for (auto i = 0; i < alpha_a; i++) {
-    double sum_i = -log(beta_a + i) - logbeta(1 + i, beta_a) - logbeta_ac_bc;
+    double sum_i = -std::log(beta_a + i) - logbeta(1 + i, beta_a) - logbeta_ac_bc;
 
     for (auto j = 0; j < alpha_b; j++) {
-      total += exp(sum_i + logbeta_ac_i_j[i + j] - log_bb_j_logbeta_j_bb[j]);
+      total += std::exp(sum_i + logbeta_ac_i_j[i + j] - log_bb_j_logbeta_j_bb[j]);
     }
   }
 
@@ -70,14 +70,14 @@ double prob_d_beats_abc(int alpha_a, int beta_a, int alpha_b, int beta_b, int al
   log_bb_j_logbeta_j_bb.reserve(alpha_b);
 
   for (auto j = 0; j < alpha_b; j++) {
-    log_bb_j_logbeta_j_bb.push_back(log(beta_b + j) + logbeta(1 + j, beta_b));
+    log_bb_j_logbeta_j_bb.push_back(std::log(beta_b + j) + logbeta(1 + j, beta_b));
   }
 
   std::vector<double> log_bc_k_logbeta_k_bc;
   log_bc_k_logbeta_k_bc.reserve(alpha_c);
 
   for (auto k = 0; k < alpha_c; k++) {
-    log_bc_k_logbeta_k_bc.push_back(log(beta_c + k) + logbeta(1 + k, beta_c));
+    log_bc_k_logbeta_k_bc.push_back(std::log(beta_c + k) + logbeta(1 + k, beta_c));
   }
 
   double abcd = beta_a + beta_b + beta_c + beta_d;
@@ -89,13 +89,13 @@ double prob_d_beats_abc(int alpha_a, int beta_a, int alpha_b, int beta_b, int al
   }
 
   for (auto i = 0; i < alpha_a; i++) {
-    double sum_i = -log(beta_a + i) - logbeta(1 + i, beta_a) - logbeta_ad_bd;
+    double sum_i = -std::log(beta_a + i) - logbeta(1 + i, beta_a) - logbeta_ad_bd;
 
     for (auto j = 0; j < alpha_b; j++) {
       double sum_j = sum_i - log_bb_j_logbeta_j_bb[j];
 
       for (auto k = 0; k < alpha_c; k++) {
-        total += exp(sum_j + logbeta_bd_i_j_k[i + j + k] - log_bc_k_logbeta_k_bc[k]);
+        total += std::exp(sum_j + logbeta_bd_i_j_k[i + j + k] - log_bc_k_logbeta_k_bc[k]);
       }
     }
   }
@@ -112,15 +112,15 @@ double prob_d_beats_abc(int alpha_a, int beta_a, int alpha_b, int beta_b, int al
 
 double prob_1_beats_2(int alpha_1, int beta_1, int alpha_2, int beta_2) {
   double total = 0.0;
-  double log_b1 = log(beta_1);
-  double a2_log_b2 = alpha_2 * log(beta_2);
-  double log_b1_b2 = log(beta_1 + beta_2);
+  double log_b1 = std::log(beta_1);
+  double a2_log_b2 = alpha_2 * std::log(beta_2);
+  double log_b1_b2 = std::log(beta_1 + beta_2);
 
   for (auto k = 0; k < alpha_1; k++) {
-    total += exp(k * log_b1 +
+    total += std::exp(k * log_b1 +
       a2_log_b2 -
       (k + alpha_2) * log_b1_b2 -
-      log(k + alpha_2) -
+      std::log(k + alpha_2) -
       logbeta(k + 1, alpha_2));
   }
 
@@ -130,17 +130,17 @@ double prob_1_beats_2(int alpha_1, int beta_1, int alpha_2, int beta_2) {
 double prob_1_beats_23(int alpha_1, int beta_1, int alpha_2, int beta_2, int alpha_3, int beta_3) {
   double total = 0.0;
 
-  double log_b1_b2_b3 = log(beta_1 + beta_2 + beta_3);
-  double a1_log_b1 = alpha_1 * log(beta_1);
-  double log_b2 = log(beta_2);
-  double log_b3 = log(beta_3);
+  double log_b1_b2_b3 = std::log(beta_1 + beta_2 + beta_3);
+  double a1_log_b1 = alpha_1 * std::log(beta_1);
+  double log_b2 = std::log(beta_2);
+  double log_b3 = std::log(beta_3);
   double loggamma_a1 = std::lgamma(alpha_1);
 
   for (auto k = 0; k < alpha_2; k++) {
     double sum_k = a1_log_b1 + k * log_b2 - std::lgamma(k + 1);
 
     for (auto l = 0; l < alpha_3; l++) {
-      total += exp(sum_k + l * log_b3
+      total += std::exp(sum_k + l * log_b3
         - (k + l + alpha_1) * log_b1_b2_b3
         + std::lgamma(k + l + alpha_1) - std::lgamma(l + 1) - loggamma_a1);
     }
